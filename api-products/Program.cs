3549eef1dad9b7377a7da64ba67a9b913bd3001b
whatSource <evenklemmetsmo@gmail.com>
2026-05-10 23:17:26 +0200

    Add product API filtering and export

---
+++ api-products/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
+
+public class Product
+{
+    public string? title { get; set; }
+    public decimal price { get; set; }
+    public string? category { get; set; }
+}
+
+public class SimpleProduct
+{
+    public string? Title { get; set; }
+    public decimal Price { get; set; }
+}
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Henter produkt data fra API...");
+
+        var url = "https://fakestoreapi.com/products";
+
+        using var client = new HttpClient();
+
+        try
+        {
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            var products = JsonSerializer.Deserialize<List<Product>>(json);
+
+            Console.WriteLine("\nProdukter:\n");
+
+
+            if (products != null)
+            {
+                var simpleProducts = new List<SimpleProduct>();
+
+                int electronicsCount = 0;
+
+                foreach (var product in products)
+                {
+                    if (product.price < 100)
+                    {
+                        Console.WriteLine($"Produkter under 100 kr: {product.title} - {product.price} kr");
+
+                        simpleProducts.Add(new SimpleProduct
+                        {
+                            Title = product.title,
+                            Price = product.price
+                        });
+                    }
+
+                    if (product.category == "electronics")
+                    {
+                        Console.WriteLine($"Elektronikk produkt: {product.title}");
+                        electronicsCount++;
+                    }
+
+                    //Console.WriteLine($"{product.title} - {product.price} kr"); 
+                }
+
+                var outputJson = JsonSerializer.Serialize(simpleProducts, new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                });
+
+                await File.WriteAllTextAsync("products.json", outputJson);
+                Console.WriteLine($"Antall elektronikk produkter: {electronicsCount}");
+                Console.WriteLine("\nproducts.json er lagret");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Feil ved henting av data: {ex.Message}");
+        }
+    }
+}
